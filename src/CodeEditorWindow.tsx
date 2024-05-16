8f5713f72618137498603d362288afa0ed32919c
sansmoraxz <31374920+sansmoraxz@users.noreply.github.com>
2024-05-17 03:33:04 +0530

    Border styles

--- src/CodeEditorWindow.tsx
+++ src/CodeEditorWindow.tsx
@@ -83,24 +83,24 @@ const CodeEditorWindow = ({ language }: CodeEditorWindowProps) => {
         <button
           onClick={() => runScript()}
           disabled={isRunning}
-          className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 mb-10"
+          className="relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 mb-10"
         >
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             Run
           </span>
         </button>
 
-        <div className="h-36 w-full overflow-y-auto">
+        <div className="h-36 overflow-x-scroll border-2 border-cyan-900 rounded-t-lg">
           <div className="text-lg  text-gray-400 pb-4 md:font-semibold">
             Output
           </div>
-          <div className="text-sm text-gray-300 md:font-light">
-            {isRunning ? "Running..." : output}
-          </div>
+          <code className="text-sm text-gray-300 md:font-light">
+            {isRunning ? "Running..." : JSON.stringify(output)}
+          </code>
         </div>
         <br />
 
-        <div className="h-fit w-full overflow-y-auto">
+        <div className="h-96 w-full overflow-y-auto border-2 border-sky-800 rounded-t-lg">
           <div className="text-lg text-gray-400 pb-4 md:font-semibold">
             Logs
           </div>
